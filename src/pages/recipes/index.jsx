e002ff49c35aead4e5228d7c876c2117955c8e2e
Ernest <ernestobimpeh9@gmail.com>
2024-02-28 10:08:57 +0000

    we got onkey enter worked

--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -5,11 +5,13 @@ import {useEffect, useState} from "react";
 export default function Recipes() {
 
     const [recipes, setRecipes] = useState([]);
+    const [searchItem, setSearchItem] =useState("");
+
     const searchRecipes = () =>{
-        //fetch recipes
         // prepare url
         const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
-        url.searchParams.append('apiKey', '2146990615544d6a87b61775f660f3d5')
+        url.searchParams.append('apiKey', process.env.REACT_APP_SPOONACULAR_API_KEY);
+        url.searchParams.append('query', searchItem)
         // fetch recipes
         fetch(url)
             .then((response) => response.json())
@@ -30,6 +32,10 @@ setRecipes(data.results);
                 id="outlined-basic"
                 label="Enter a keyword to search recipes and hit Enter"
                 variant="outlined"
+                value= {searchItem}
+                onChange={(event) => setSearchItem(event.target.value)}
+                onKeyDown={event => event.key == 
+                'Enter'&& searchRecipes()}
             />
             <Grid sx={{ mt: '1rem' }} container spacing={3}>
                 {recipes.map((recipe) => <RecipeItem key={recipe.id} title={recipe.title} image={recipe.image} />)}
